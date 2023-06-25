8da5c2fc50b24156ac097b88cae70874a2c48e21
sgvonbergen <sgvonbergen@uc.cl>
2023-06-25 00:07:27 -0400

    can now remove countries from list

--- scripts/linegraph.js
+++ scripts/linegraph.js
@@ -21,8 +21,25 @@ const LINEMARGIN = {
   right: 200
 }
 
+const crossShape = d3.symbol(d3.symbolsStroke[2])
+const emptyCountry = {
+  Rank: 0,
+  CCA3: "",
+  Country: "Add Country",
+  color: "#FFFFFF",
+  popdata: [
+    { date: 1970, population: 0 },
+    { date: 1980, population: 0 },
+    { date: 1990, population: 0 },
+    { date: 2000, population: 0 },
+    { date: 2010, population: 0 },
+    { date: 2015, population: 0 },
+    { date: 2020, population: 0 },
+    { date: 2022, population: 0 }
+  ]
+}
 const MAXPOP =  1500000000
-const LineAnimationDelay = 2000
+const LineAnimationDelay = 500
 const TimeStart = 1970
 const TimeEnd = 2022
 const LineWidthScale = d3
@@ -97,59 +114,18 @@ const zeroLine = d3.line()
   .x(d => LineWidthScale(d.date))
   .y(LINEHEIGHT-LINEMARGIN.bot)
 
-
-function parseLineData(data) {
-  const lineData = []
-  let i = 0
-  data.forEach(country => {
-    let popbydate = []
-    Object.keys(country.Population).forEach( year => {
-      popbydate.push({
-        date: year,
-        population: country.Population[year]
-      })
-    })
-    lineData.push({
-      Country: country.Country,
-      Rank: country.Rank,
-      CCA3: country.CCA3,
-      popdata: popbydate,
-      color: LineColors[i]
-    })
-    i = ++i % 8
-  })
-  return lineData.sort((a, b) => a.popdata[2022] - b.popdata[2022])
-}
-
-function parseRowCSV(d) {
-  const data = {
-    Rank: +d.Rank,
-    CCA3: d.CCA3,
-    Country: d.Country,
-    popdata: [
-      {date:1970, population: +d['1970']},
-      {date:1980, population: +d['1980']},
-      {date:1990, population: +d['1990']},
-      {date:2000, population: +d['2000']},
-      {date:2010, population: +d['2010']},
-      {date:2015, population: +d['2015']},
-      {date:2020, population: +d['2020']},
-      {date:2022, population: +d['2022']},
-    ],
-    color: LineColors[i]
-  }
-  i = ++i % 8
-  return data
-}
-
+var selectedCountries = []
 
 function makeLineGraph(data) {
-  const selectedCountries = data.slice(0, 8)
-  SVGLINE.selectAll("path.country-line")
-    .data(selectedCountries)
+  SVGLINE.selectAll("g.country-line-group")
+    .data(data, d => d.popdata)
     .join(
      enter => {
-      let g = enter;
+      const g = enter.append("g")
+        .attr("id", d => `line-group-${d.Rank}`)
+        .attr("class", "country-line-group")
+        .attr("opacity", d => Number(!(d.Rank == 0)))
+
       g.append("path")
       .attr("class", "country-line")
       .attr("stroke", d => d.color)
@@ -158,123 +134,188 @@ function makeLineGraph(data) {
       .duration(LineAnimationDelay)
       .attr("d", d => line(d.popdata))
       
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[0].population))
-      .attr("cx", LineWidthScale(1970))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[1].population))
-      .attr("cx", LineWidthScale(1980))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[2].population))
-      .attr("cx", LineWidthScale(1990))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[3].population))
-      .attr("cx", LineWidthScale(2000))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[4].population))
-      .attr("cx", LineWidthScale(2010))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[5].population))
-      .attr("cx", LineWidthScale(2015))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[6].population))
-      .attr("cx", LineWidthScale(2020))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
-      g.append("circle")
-      .attr("class", "line-marker")
-      .attr("r", 0)
-      .attr("fill", d => d.color)
-      .attr("cy", d => LineHeightScale(d.popdata[7].population))
-      .attr("cx", LineWidthScale(2022))
-      .transition()
-      .delay(LineAnimationDelay)
-      .duration(500)
-      .attr("r", 5)
-
+      g.selectAll("circle.line-marker")
+        .data(d => d.popdata)
+        .join("circle")
+        .attr("class", "line-marker")
+        .attr("r", 0)
+        .attr("fill", d => d.color)
+        .attr("cy", d => LineHeightScale(d.population))
+        .attr("cx", d => LineWidthScale(d.date))
+        .transition()
+        .delay(LineAnimationDelay)
+        .duration(500)
+        .attr("r", 5)
      },
-    exit => exit
-      .transition()
-      .duration(1000)
-      .attr("d", d => zeroLine(d.popdata))
+
+    update => {
+      update.attr("id", d => `line-group-${d.Rank}`).attr("opacity", d => Number(!(d.Rank == 0)))
+      update.call(g => g.selectAll("circle.line-marker")
+        .transition()
+        .call(d => console.log(d))
+        .duration(300)
+        .attr("r", 0)
+        .transition()
+        .duration(1)
+        .attr("fill", d => d.color)
+        .attr("cy", d => LineHeightScale(d.population))
+        .attr("cx", d => LineWidthScale(d.date))
+        .transition()
+        .delay(LineAnimationDelay)
+        .duration(500)
+        .attr("r", 5)
+      )
+      update.call(g => g.select("path.country-line")
+        .transition()
+        .duration(1)
+        .attr("d", d => zeroLine(d.popdata))
+        .transition()
+        .duration(500)
+        .attr("d", d => line(d.popdata))
+        .attr("stroke", d => d.color)
+      )
+    }
+  )
+
+  SVGLINE.selectAll("g.country-legend")
+    .data(data)
+    .join(
+      enter => {
+        const g = enter.append("g")
+          .attr("class", "country-legend")
+          .attr("id", d => `legendcontainer-${d.Rank}`)
+
+        g.append("circle")
+          .attr("class", "country-legend-color")
+          .attr("id", d => `Circle-${d.Rank}`)
+          .attr("r", 10)
+          .attr("cx", LINEWIDTH - 150)
+          .attr("cy", (d, i) => 130 + 30 * i)
+          .attr("fill", d => d.color)
+          .on("click", removeCountry)
+          .on("mouseover", showCross)
+          .on("mouseout", hideCrosses)
+        
+        g.append("text")
+          .text(d => d.Country)
+          .attr("class", "country-legend-country-name")
+          .attr("x", LINEWIDTH - 120)
+          .attr("y", (d, i) => 135 + 30 * i)
+
+        g.append("path")
+          .attr("class", "country-legend-cross")
+          .attr("id", d => `Cross-${d.Rank}`)
+          .attr("d", crossShape)
+          .attr("transform", (d, i) => {
+            if (d.Rank == 0) {
+              return `translate(${LINEWIDTH - 150},${130 + 30 * i}) rotate(45)`
+            } else {
+              return `translate(${LINEWIDTH - 150},${130 + 30 * i})`
+            }
+          })
+          .attr("opacity", 0)
+          .on("mouseover", showCross)
+          .on("click", (d, i) => {
+            if (d.Rank == 0) {
+              addCountry(d, i)
+            } else {
+              removeCountry(d, i)
+            }
+          })
+        
+      },
+      update => {
+        update.call(g => g.select("circle.country-legend-color")
+          .attr("fill", d => d.color)
+          .attr("id", d => `Circle-${d.Rank}`)
+          .on("click", removeCountry)
+          .on("mouseover", showCross)
+          .on("mouseout", hideCrosses)
+        )
+        update.call(g => g.select("text.country-legend-country-name")
+          .text(d => d.Country)
+        )
+        update.call(g => g.select("path.country-legend-cross")
+          .attr("id", d => `Cross-${d.Rank}`)
+          .attr("transform", (d, i) => {
+            if (d.Rank == 0) {
+              return `translate(${LINEWIDTH - 150},${130 + 30 * i}) rotate(45)`
+            } else {
+              return `translate(${LINEWIDTH - 150},${130 + 30 * i})`
+            }
+          })
+          .on("mouseover", showCross)
+          .on("click", (d, i) => {
+            if (d.Rank == 0) {
+              addCountry(d, i)
+            } else {
+              removeCountry(d, i)
+            }
+          })
+        )
+        update.attr("id", d => `legendcontainer-${d.Rank}`)
+      }
     )
-  
+    
+}
 
-  SVGLINE.selectAll("circle.country-legend-color")
-    .data(selectedCountries)
-    .join("circle")
-    .attr("class", "country-legend-color")
-    .attr("r", 5)
-    .attr("cx", LINEWIDTH - 150)
-    .attr("cy", d => 150 + 40 * data.indexOf(d))
-    .attr("fill", d => d.color)
-  
-  SVGLINE.selectAll("text.country-legend-country-name")
-    .data(selectedCountries)
-    .join("text")
-    .text(d => d.Country)
-    .attr("class", "country-legend-country-name")
-    .attr("x", LINEWIDTH - 120)
-    .attr("y", d => 150 + 40 * data.indexOf(d))
+function removeCountry(d, i) {
+  const toRemoveIndex = selectedCountries.indexOf(i)
+  selectedCountries.splice(toRemoveIndex, 1)
+  if (!selectedCountries.includes(emptyCountry)) { 
+    selectedCountries.push(emptyCountry)
+  }
+  makeLineGraph(selectedCountries)
+}
 
+function addCountry(d, i) {
+  console.log("I want to add a country")
+}
+
+function showCross(d, i) {
+  let crossId = d.target.parentNode.id.split("-")[1]
+  SVGLINE.select(`path#Cross-${crossId}`)
+    .interrupt("hideCrosses")
+    .transition("showCross")
+    .duration(100)
+    .attr("opacity", 1)
+    
+}
+
+function hideCrosses(d, i) {
+  SVGLINE.selectAll("path.country-legend-cross")
+    .transition("hideCrosses")
+    .delay(200)
+    .duration(100)
+    .attr("opacity", 0)
 }
 
 
+function parseRowCSV(d) {
+  const data = {
+    Rank: +d.Rank,
+    CCA3: d.CCA3,
+    Country: d.Country,
+    popdata: [
+      {date:1970, population: +d['1970'], color: LineColors[i]},
+      {date:1980, population: +d['1980'], color: LineColors[i]},
+      {date:1990, population: +d['1990'], color: LineColors[i]},
+      {date:2000, population: +d['2000'], color: LineColors[i]},
+      {date:2010, population: +d['2010'], color: LineColors[i]},
+      {date:2015, population: +d['2015'], color: LineColors[i]},
+      {date:2020, population: +d['2020'], color: LineColors[i]},
+      {date:2022, population: +d['2022'], color: LineColors[i]},
+    ],
+    color: LineColors[i]
+  }
+  i = ++i % 8
+  return data
+}
+
 let i = 0;
 d3.csv("data/population.csv", parseRowCSV).then((populationdata) => {
-  makeLineGraph(populationdata)
+  selectedCountries = populationdata.slice(0, 8)
+  console.log("Loaded 8 most populous countries for line graph")
+  selectedCountries.push(emptyCountry)
+  makeLineGraph(selectedCountries)
 })
